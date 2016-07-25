35fb6b482b0d695224f7f1bb731295d29535799e
dvwhitem <dvwhitem@gmail.com>
2016-07-25 20:48:25 +0300

    initial commit

---
+++ src/main/java/com/home/services/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.home.services.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by vitaliy on 7/25/16.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("message", "World");
+        return "index";
+    }
+}
